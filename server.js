04ffcaf354a6c2e8dca9422197dd41e07e3889f2
aznjp <jpark103193@gmail.com>
2020-10-15 18:12:48 -0400

    added in the server js post and delete methods for api

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // Make constant express to call on the module
 const express = require("express");
 const fs = require("fs")
+const path = require('path')
+const { v4: uuidv4 } = require('uuid');
+
+let notes = require('./db/db.json')
 
 // Use app constant variable for express method
 const app = express();
@@ -32,15 +36,34 @@ app.get("/notes", function(req, res) {
 
 // ======== THIS IS THE API ROUTES ================
 app.get("/api/notes", function(req, res) {
-
+    res.json(notes)
 });
 
 app.post("/api/notes", function(req, res) {
+    console.log(req.body)
+        // This modules will add in new id to the req.body using the uuid module
+    req.body.id = uuidv4();
 
+    // Push back in the information logged in back into the notes array
+    // Then send back response in JSON format
+    notes.push(req.body)
+        // Would normally use writeFile for asynchronous functionality but for simplicity sake for a small project like this it will do.
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes)
 });
 
 app.delete("/api/notes/:id", function(req, res) {
-
+    req.params.id
+        // filter method will return new array thus the need to either set empty array to equal
+    notes = notes.filter(n => {
+        if (req.params.id === n.id) {
+            return false
+        } else {
+            return true
+        }
+    })
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes)
 });
 // ======== END OF API ROUTES ======================
 
