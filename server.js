69790861bb819a69a04ea401ac5576bbcce267cd
aznjp <jpark103193@gmail.com>
2020-10-15 17:17:05 -0400

    set up html routes

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const apiRoutes = require('./routes/apiRoutes.js');
-const htmlRoutes = require('./routes/htmlRoutes.js');
-
 // Make constant express to call on the module
 const express = require("express");
+const fs = require("fs")
 
 // Use app constant variable for express method
 const app = express();
@@ -14,13 +12,37 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Requires the app GET and POST methods out of routes folder
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
 // Instructs the server to make these files static resources. 
 // This means that all of our front-end code can now be accessed without having a specific server endpoint created for it!
 app.use(express.static("public"));
+
+
+// ======== THIS IS THE HTML ROUTES ================
+// Route to go to INDEX page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// Route to go to NOTES page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+// ======== END OF HTML ROUTES ======================
+
+
+// ======== THIS IS THE API ROUTES ================
+app.get("/api/notes", function(req, res) {
+
+});
+
+app.post("/api/notes", function(req, res) {
+
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+
+});
+// ======== END OF API ROUTES ======================
 
 // Starts the server to listen for requests
 app.listen(PORT, function() {
